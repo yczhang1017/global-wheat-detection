8a6bee1718cf40662d452385dc62339d15a9beb3
yczhang1017 <zhangyuchen12344@gmail.com>
2020-07-17 12:58:17 -0400

    Add files via upload

--- create_labels.py
+++ create_labels.py
@@ -13,32 +13,27 @@ for f in ['images', 'labels']:
 image_folder = 'images'
 dfa = pd.read_csv('train.csv',index_col=0)
 box_count = dfa.groupby(level=0).count().width
-all_id = list(box_count.index)
+#all_id = list(box_count.index)
+all_id = list(set([i[:-4] for i in os.listdir('all_images')]))
 idsets = train_test_split(all_id, test_size=0.1, random_state=7)
     
-def writelabel(iid,row,p):
-    wi,hi, bbox = row.width, row.height, row.bbox
-    xb,yb,wb,hb =ast.literal_eval(bbox)      
-    yolo_row = [0,(xb+wb/2)/wi,(yb+hb/2)/hi,wb/wi,hb/hi]
-    labelfile = os.path.join('labels',p,iid+'.txt')
-    with open(labelfile,'a') as fw:
-        fw.write(' '.join([str(c) for c in yolo_row])+'\n')
-        
-        
+
 for p, ids in zip(phases, idsets):
     i=0
     for iid in ids:
         imagefile1 = os.path.join('all_images',iid+'.jpg')
         imagefile2 = os.path.join('images',p,iid+'.jpg')
-        try:
-            copyfile(imagefile1,imagefile2)
-        except:
-            print(f'{imagefile1} does not exist')
+        copyfile(imagefile1,imagefile2)
         
+        if iid not in df.index: continue
         idf = dfa.loc[iid]
-        if isinstance(idf, pd.Series):
-            writelabel(iid,idf,p)
-            continue
-        for _,row in idf.iterrows():
-            writelabel(iid,row,p)
+        if isinstance(idf, pd.Series):  dfa.loc[[iid]]
+        with open(os.path.join('labels',p,iid+'.txt'),'w') as fw:
+            for _,row in idf.iterrows():
+                wi,hi, bbox = row.width, row.height, row.bbox
+                xb,yb,wb,hb =ast.literal_eval(bbox)      
+                yolo_row = [0,(xb+wb/2)/wi,(yb+hb/2)/hi,wb/wi,hb/hi]
+                fw.write(' '.join([str(c) for c in yolo_row])+'\n')
+        #i+=1
+        #if i>10: break
 
