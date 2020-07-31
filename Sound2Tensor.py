84c7e975aa02727f83fa13a4d4bb9d3dff83d727
yczhang1017 <zhangyuchen12344@gmail.com>
2020-07-31 01:31:09 -0400

    Update Sound2Tensor.py

--- Sound2Tensor.py
+++ Sound2Tensor.py
@@ -5,39 +5,46 @@ import sys
 import numpy as np
 import torch
 from tqdm import tqdm
+import pandas as pd
 mel_parameters = {
     "fmin": 40,
     "fmax": 14000
 }    
 
 
-def sound2tensor(filename,ndim=128, sr = 44100):
-    audio = AudioSegment.from_mp3(filename)
+def sound2tensor(filename, form='mp3', ndim=128, sr = 44100):
+    if form== 'mp3':
+        audio = AudioSegment.from_wp3(filename)
+    elif form == 'wav': 
+        audio = AudioSegment.from_wav(filename)
     sample = audio.set_channels(1).set_frame_rate(sr).get_array_of_samples()
+    
     x = np.array(sample).astype(np.float32)/2**14
     S = librosa.feature.melspectrogram(x,sr,n_mels=ndim,**mel_parameters)
     Sdb = librosa.power_to_db(S).astype(np.float32).transpose()
     return torch.tensor(Sdb)
 
+def df2tensor(df,form='mp3'):
+    for r in tqdm(df.iterrows(), total = len(df1)):
+        t = r['ebird_code']
+        f = r['filename'] 
+        x = sound2tensor(os.path.join('train_audio',t,f), form)
+        torch.save(x, os.path.join(save, f[:-3]+'pt'))
+            
+
 root = sys.argv[1] if len(sys.argv)>1 else './'
 os.chdir(root)
 save = 'tensors'
-if not os.path.exists(save):
-    os.mkdir(save)
-for t in tqdm(os.listdir('train_audio')):
-    for f in os.listdir(os.path.join('train_audio',t)):
-        try:
-            x = sound2tensor(os.path.join('train_audio',t,f))
-        except: 
-            print(f'{f} cannot be decoded')
-        torch.save(x, os.path.join(save, f[:-3]+'pt'))
+df = pd.read_csv('train.csv')
+df1 = [df['file_type'] == 'wav']
+df2 = [df['file_type'] != 'wav']
+df2tensor(df1,'wav')
+#df2tensor(df2,'mp3')
 
+"""    
 for f in os.listdir('example_test_audio'):
-    try:
-        x = sound2tensor(os.path.join('example_test_audio',f))
-    except: 
-        print(f'{f} cannot be decoded')
+    x = sound2tensor(os.path.join('example_test_audio',f))
     torch.save(x, os.path.join(save, f[:-3]+'pt'))
 
-
+"""
     
