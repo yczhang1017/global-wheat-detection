830aa1a1e9eeb24af92e8e58b63c563aafc095d1
yczhang1017 <zhangyuchen12344@gmail.com>
2020-08-11 02:54:05 -0400

    Update bird_train_cnn.py

--- bird_train_cnn.py
+++ bird_train_cnn.py
@@ -95,7 +95,7 @@ class TrainData(Dataset):
         return x,t
 
 
-def adjust_learning_rate(optimizer, e, lr0=1e-8, warmup=1, Tmax=epoch-1):
+def adjust_learning_rate(optimizer, e, lr0=1e-3, warmup=1, Tmax=epoch-1):
     if e <= warmup:
         lr = lr0
     else:
